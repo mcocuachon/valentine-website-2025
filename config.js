727839e07f2bcf01be8c0561157b2850d09fe44e
mcocuachon <mcocuachon@gmail.com>
2026-02-01 23:00:34 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dkmuyxj2m/video/upload/v1769957766/Fitterkarma_-_Kalapastangan_Trend_Lyrics_Loop_hoav3s.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dkmuyxj2m/video/upload/Fitterkarma_-_Kalapastangan_Trend_Lyrics_Loop_hoav3s.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
